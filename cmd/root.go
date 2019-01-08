b02fc6e162d1dd25651a2c35fc7d75e3a72c54a9
it_akumi <it.akumi1011@gmail.com>
2019-01-08 23:37:05 +0900

    Add init and initConfig

--- cmd/root.go
+++ cmd/root.go
@@ -3,10 +3,22 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"os"
 )
 
 var rootCmd = &cobra.Command{}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
