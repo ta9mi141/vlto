8683d34271282823de800948c4f2298034e1db33
it_akumi <it.akumi1011@gmail.com>
2019-02-02 22:45:05 +0900

    Add Unmarshal

--- project/project.go
+++ project/project.go
@@ -1,12 +1,27 @@
 package project
 
 import (
+	"github.com/spf13/viper"
 	"time"
 )
 
+type Config struct {
+	Name          string
+	Goal          int
+	StartDate     time.Time
+	IterationDays int
+}
+
 type Project struct {
-	Name      string
-	Goal      int
-	StartDate time.Time
-	Iteration int
+	Name                  string
+	Goal                  int
+	TotalAchievedHour     int
+	IterationAchievedHour int
+	LastDay               time.Time
+}
+
+func Unmarshal() []Config {
+	var configs []Config
+	viper.UnmarshalKey("Projects", &configs)
+	return configs
 }
