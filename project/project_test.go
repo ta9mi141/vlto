16c1825386c490994e652db47b40f2c5d6626a62
it_akumi <it.akumi1011@gmail.com>
2019-02-06 22:16:49 +0900

    Add TestDivideElapsedYears

--- project/project_test.go
+++ project/project_test.go
@@ -1,6 +1,7 @@
 package project
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -14,6 +15,29 @@ func TestGetIterationSpan(t *testing.T) {
 	}
 	iterationSpan := getIterationSpan(today, 14)
 	if iterationSpan != expected {
+		t.Error()
+	}
+}
+
+func TestDivideElapsedYears(t *testing.T) {
+	today := time.Date(2019, 2, 5, 0, 0, 0, 0, time.Local)
+	startDate := time.Date(2016, 10, 11, 0, 0, 0, 0, time.Local)
+	expected := []dateSpan{
+		{
+			since: startDate,
+			until: time.Date(2017, 10, 11, 0, 0, 0, 0, time.Local),
+		},
+		{
+			since: time.Date(2017, 10, 12, 0, 0, 0, 0, time.Local),
+			until: time.Date(2018, 10, 12, 0, 0, 0, 0, time.Local),
+		},
+		{
+			since: time.Date(2018, 10, 13, 0, 0, 0, 0, time.Local),
+			until: today,
+		},
+	}
+	elapsedYears := divideElapsedYears(startDate)
+	if !reflect.DeepEqual(elapsedYears, expected) {
 		t.Error()
 	}
 }
