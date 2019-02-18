8a3d2d150136cd2d602954b5e7cc21acf9e5f89d
it_akumi <it.akumi1011@gmail.com>
2019-02-18 23:30:25 +0900

    Make default config file path constant

--- config/config.go
+++ config/config.go
@@ -2,11 +2,23 @@ package config
 
 import (
 	"github.com/spf13/viper"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	defaultConfigFilePath string = "$HOME/.config/vlto.toml"
 )
 
 func Init(cfgFilePath string) error {
-	viper.AddConfigPath("$HOME/.config") // Adding $HOME/.config as first search path
-	viper.SetConfigName("vlto")          // Name of config file (without extention)
+	// Adding a directory of defaultConfigFilePath as first search path
+	viper.AddConfigPath(filepath.Dir(defaultConfigFilePath))
+
+	// Name of config file (without extention)
+	viper.SetConfigName(strings.TrimSuffix(
+		filepath.Base(defaultConfigFilePath),
+		filepath.Ext(defaultConfigFilePath),
+	))
 	if cfgFilePath != "" {
 		viper.SetConfigFile(cfgFilePath)
 	}
