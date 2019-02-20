67eb59dd21e2237b2fe6fef60fddbd209cf708fb
it_akumi <it.akumi1011@gmail.com>
2019-02-20 19:14:22 +0900

    Add TestInitDefaultConfigFilePath

--- config/config_test.go
+++ config/config_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -23,12 +24,30 @@ StartDate = 2019-01-01T00:00:00+00:00
 IterationDays = 14
 `
 
+func TestInitDefaultConfigFilePath(t *testing.T) {
+	defaultConfigFilePath := strings.Replace(
+		defaultConfigFilePath, "$HOME", os.Getenv("HOME"), -1,
+	)
+	err := ioutil.WriteFile(defaultConfigFilePath, []byte(config), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(defaultConfigFilePath)
+
+	if err := Init(""); err != nil {
+		t.Error(err)
+	}
+}
+
 func TestInitCustomConfigFilePath(t *testing.T) {
 	customConfigFilePath := "vlto.toml"
-	ioutil.WriteFile(customConfigFilePath, []byte(config), 0644)
+	err := ioutil.WriteFile(customConfigFilePath, []byte(config), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer os.Remove(customConfigFilePath)
 
 	if err := Init(customConfigFilePath); err != nil {
-		t.Error()
+		t.Error(err)
 	}
 }
